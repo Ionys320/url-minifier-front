48853f27ab8aca90ff7ff03e17b5bf97fe770d27
Ionys <9364594+Ionys320@users.noreply.github.com>
2023-09-26 22:00:18 +0200

    Navigation: Make sure the menu is keeping it size

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col lg:flex-row gap-8 h-full">
         <!-- Left/top navigation bar -->
-        <nav class="flex gap-2 lg:flex-col lg:basis-60 bg-gray-700 rounded-lg lg:h-full p-4">
+        <nav class="flex gap-2 lg:flex-col lg:basis-60 shrink-0 bg-gray-700 rounded-lg lg:h-full p-4">
             <NavItem :title="'URLs'" :url="'/'" :icon="'link'" />
             <NavItem :title="'Users'" :url="'/users'" :icon="'users'" />
         </nav>
